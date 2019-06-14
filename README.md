ba82703336bf463cba792bc7ff27d23085dec3be
Dwi Prasetyo Adi Nugroho <dwiprasetyo.an@gmail.com>
2019-06-14 17:50:08 +0200

    Add get category by product name endpoint

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # reachit-analytic
 Analytic Service for ReachIT
+
+This instruction assumes that you already have Python 3 installed.
+
+Install dependencies by running pip install -r requirements.txt.
+Copy .env.example as .env.
+Set the environment variables in .env according to your system configuration.
+To run it locally execute python app.py.
---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from SPARQLWrapper import SPARQLWrapper, JSON
+import os
+from dotenv import load_dotenv
+from flask import request
+from flask import abort
+
+
+app = Flask(__name__)
+load_dotenv()
+
+# Set Up SPARQL Connection
+sparql = SPARQLWrapper(os.getenv('SPARQL_ENDPOINT'))
+sparql.setReturnFormat(JSON)
+sparql.addDefaultGraph('http://localhost:8890/reach-it')
+
+@app.route('/productcategory/', methods=['GET'])
+def get_product_category():
+    product_name = request.args.get('product_name')
+    print(product_name)
+    """Get category of product given product name.
+    Args:
+        product_name (str): The product name. It should be url encoded, for example "Charcoal, sack" -> Charcoal%2C%20sack
+
+    Returns:
+        str: The category of queried product name.
+    """
+    q = ( f"""
+    PREFIX reachIT: <http://www.reach-it.com/ontology/>
+    SELECT str(?c) as ?type
+        WHERE {{
+                ?product reachIT:productName ?productName.
+                ?product reachIT:belongsToCategory ?t.
+                ?t reachIT:categoryName ?c .
+                FILTER (lang(?c)='en' and lang(?productName)='en' and ?productName="{product_name}"@en)
+        }}
+    ORDER BY  DESC(?type)
+    LIMIT 1
+    """)
+    print(q)
+    sparql.setQuery(q)
+    query_results = sparql.query().convert()
+    try:
+        return query_results["results"]["bindings"][0]["type"]["value"]
+    except IndexError:
+        return abort(404)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
